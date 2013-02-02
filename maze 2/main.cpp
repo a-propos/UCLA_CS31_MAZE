96b042bae3c5dcec9aa2d7381a77274e471d39e0
Panos Karagiannis <Panos@s183-202.resnet.ucla.edu>
2013-02-02 05:26:24 +0200

    Initial Commit

---
+++ maze 2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  maze 2
+//
+//  Created by Panos Karagiannis on 2/2/13.
+//  Copyright (c) 2013 Panos Karagiannis. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
